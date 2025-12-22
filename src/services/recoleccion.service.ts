ea6f5458b867e6bd0b7fab6adbc9d64a1c483137
jhamilcali <jhamilquea@gmail.com>
2025-12-22 15:33:32 -0400

    solucionanndo problemas de input

--- src/services/recoleccion.service.ts
+++ src/services/recoleccion.service.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend-nestjs-production-c419.up.railway.app';
 
 // ===== TIPOS =====
 export interface CreateRecoleccionDto {
