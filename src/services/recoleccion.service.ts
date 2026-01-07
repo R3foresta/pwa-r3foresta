4ef144fbfc61fd19cd1a06d4d38cbbbe75667330
jhamilcali <jhamilquea@gmail.com>
2026-01-07 10:27:11 -0400

    cambio de back

--- src/services/recoleccion.service.ts
+++ src/services/recoleccion.service.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://backend-nestjs-production-c419.up.railway.app';
+const API_URL = import.meta.env.VITE_API_URL || 'https://back-reforesta.onrender.com';
 
 // ===== TIPOS =====
 export interface CreateRecoleccionDto {
