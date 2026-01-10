2709ceb98692a494f260058de0c59bb2d72e7b10
jhamilcali <jhamilquea@gmail.com>
2026-01-09 23:06:02 -0400

    cambios de back a vercel v2

--- src/services/recoleccion.service.ts
+++ src/services/recoleccion.service.ts
@@ -1,5 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://backend-r3foresta.vercel.app';
-
+//const API_URL = import.meta.env.VITE_API_URL || 'https://backend-r3foresta.vercel.app';
+const API_URL = 'https://backend-r3foresta.vercel.app';
 // ===== TIPOS =====
 export interface CreateRecoleccionDto {
   fecha: string; // YYYY-MM-DD
