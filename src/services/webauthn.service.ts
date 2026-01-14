3e4c1ee7a7b17155826a982112f0dadaa0b1956e
jhamilcali <jhamilquea@gmail.com>
2026-01-14 10:56:37 -0400

    cambiando url y sol error de @passwordless-id/webauthn

--- src/services/webauthn.service.ts
+++ src/services/webauthn.service.ts
@@ -1,7 +1,8 @@
-import { client } from '@passwordless-id/webauthn';
 import type { AuthResponse, ChallengeResponse } from '../types/auth.types';
+import { client } from '@passwordless-id/webauthn';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend-r3foresta.onrender.com'
+// const API_URL = import.meta.env.VITE_API_URL;
 // const API_URL = 'https://backend-r3foresta.onrender.com';
 
 
